1a6ad6c73ed8d61cb1a00f557a73e464607423ae
008adarshshukla <shuklaadarsh1932@gmail.com>
2023-04-21 11:10:46 +0530

    Initial Commit

---
+++ StoryApp/StoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryAppApp.swift
+//  StoryApp
+//
+//  Created by Adarsh Shukla on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
